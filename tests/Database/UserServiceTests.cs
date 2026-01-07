9d4b5fda6f40fa6fa418aae696b4186dbfec6051
MyGitHubBlueberry <namespaceblueberry@gmail.com>
2026-01-07 14:51:05 +0100

    fix: tests connect to inmemory db instead of physical one

--- tests/Database/UserServiceTests.cs
+++ tests/Database/UserServiceTests.cs
@@ -14,7 +14,7 @@ public class UserServiceTests
         string password = "123";
 
         using (connection) {
-            using (var context = new Context()) {
+            using (var context = new Context(options)) {
                 var service = new UserService(context, CancellationToken.None);
 
                 var user = await service.CreateUserAsync(username, password);
@@ -23,7 +23,7 @@ public class UserServiceTests
                 Assert.Equal(user.Username, username);
             }
 
-            using (var context = new Context()) {
+            using (var context = new Context(options)) {
                 var savedUser = await context.Users
                     .FirstOrDefaultAsync(u => u.Username == username);
 
@@ -32,4 +32,31 @@ public class UserServiceTests
             }
         }
     }
+
+    [Fact]
+    public async Task Cant_Create_Duplicate_User()
+    {
+        var (connection, options) = await DbTestHelper.SetupTestDbAsync();
+        string username = "taken";
+        string password = "doesn't matter";
+        using (connection) 
+        {
+            using (var context = new Context(options)) 
+            {
+                var service = new UserService(context, CancellationToken.None);
+                var user = await service.CreateUserAsync(username, password);
+
+                Assert.NotNull(user);
+                Assert.Equal(user.Username, username);
+            }
+
+            using (var context = new Context(options)) 
+            {
+                var service = new UserService(context, CancellationToken.None);
+                var user = await service.CreateUserAsync(username, password);
+                Assert.Null(user);
+            }
+        }
+    }
+
 }
