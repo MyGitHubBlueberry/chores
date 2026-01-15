bfa7c2d615271667acda8b3395a450b6378cd9d0
MyGitHubBlueberry <namespaceblueberry@gmail.com>
2026-01-15 16:23:44 +0100

    feat: added end date to chore

---
+++ server/Database/Migrations/20260115152254_ChoreEndDateAdded.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChoreEndDateAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "Chores",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "Chores");
+        }
+    }
+}
