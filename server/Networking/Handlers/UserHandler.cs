ae636cb482cac3b85ec10cf9cc157ec986f484c4
MyGitHubBlueberry <namespaceblueberry@gmail.com>
2026-01-23 18:05:45 +0100

    feat: handled other user requests

--- server/Networking/Handlers/UserHandler.cs
+++ server/Networking/Handlers/UserHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics;
 using System.Text.Json;
 using System.Threading;
@@ -17,13 +18,73 @@ public class UserHandler(UserService service) : IPacketHandler
         {
             switch (packet.code)
             {
-                case OpCode.DeleteUser:
-                    return await HandleUserDeletionAsync(context, packet, token);
+                case OpCode.GetAssociatedLogs:
+                    return await HandleGetAssociatedLogsAsync(context, packet, token);
+                case OpCode.GetMemberships:
+                    return await HandleGetMembershipsAsync(context, packet, token);
+                case OpCode.GetOwnedChores:
+                    return await HandleGetOwnedChoresAsync(context, packet, token);
+                case OpCode.GetUserByName:
+                    return await HandleGetUserByNameAsync(context, packet, token);
                 case OpCode.GetUserById:
                     return await HandleGetUserByIdAsync(context, packet, token);
+                case OpCode.DeleteUser:
+                    return await HandleUserDeletionAsync(context, packet, token);
             }
         }
         return false;
+    }
+
+    private async Task<bool> HandleGetAssociatedLogsAsync
+        (ClientContext context, ReadPacket packet, CancellationToken token)
+    {
+        var request = JsonSerializer.Deserialize<GetLogsByUserIdRequest>(packet.jsonData);
+        Debug.Assert(request is not null);
+        SendPacket<Result> sendPacket;
+        var result = await service
+            .GetAssociatedLogsByIdAsync(request.UserId);
+        sendPacket = new(packet.code, result);
+        await PacketProtocol.SendPacketAsync(context.Stream, sendPacket);
+        return true;
+    }
+
+    private async Task<bool> HandleGetMembershipsAsync
+        (ClientContext context, ReadPacket packet, CancellationToken token)
+    {
+        var request = JsonSerializer.Deserialize<GetMembershipsByIdRequest>(packet.jsonData);
+        Debug.Assert(request is not null);
+        SendPacket<Result> sendPacket;
+        var result = await service
+            .GetMembershipsByIdAsync(request.UserId);
+        sendPacket = new(packet.code, result);
+        await PacketProtocol.SendPacketAsync(context.Stream, sendPacket);
+        return true;
+    }
+
+    private async Task<bool> HandleGetOwnedChoresAsync
+        (ClientContext context, ReadPacket packet, CancellationToken token)
+    {
+        var request = JsonSerializer.Deserialize<GetOwnedChoresByIdRequest>(packet.jsonData);
+        Debug.Assert(request is not null);
+        SendPacket<Result> sendPacket;
+        var result = await service
+            .GetOwnedChoresByIdAsync(request.UserId);
+        sendPacket = new(packet.code, result);
+        await PacketProtocol.SendPacketAsync(context.Stream, sendPacket);
+        return true;
+    }
+
+    private async Task<bool> HandleGetUserByNameAsync
+        (ClientContext context, ReadPacket packet, CancellationToken token)
+    {
+        var request = JsonSerializer.Deserialize<GetUserByNameRequest>(packet.jsonData);
+        Debug.Assert(request is not null);
+        SendPacket<Result> sendPacket;
+        var result = await service
+            .GetByNameAsync(request.Username);
+        sendPacket = new(packet.code, result);
+        await PacketProtocol.SendPacketAsync(context.Stream, sendPacket);
+        return true;
     }
 
     private async Task<bool> HandleGetUserByIdAsync
@@ -32,12 +93,6 @@ public class UserHandler(UserService service) : IPacketHandler
         var request = JsonSerializer.Deserialize<GetUserByIdRequest>(packet.jsonData);
         Debug.Assert(request is not null);
         SendPacket<Result> sendPacket;
-        if (context.CurrentUser is null)
-        {
-            sendPacket = new(packet.code, Result.Fail(ServiceError.Conflict, "Log in first."));
-            await PacketProtocol.SendPacketAsync(context.Stream, sendPacket);
-            return true;
-        }
         var result = await service
             .GetByIdAsync(request.UserId);
         sendPacket = new(packet.code, result);
