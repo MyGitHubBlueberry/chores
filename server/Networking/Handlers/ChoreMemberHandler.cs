638a3de80301297fdad73658baecd9e84f715614
MyGitHubBlueberry <namespaceblueberry@gmail.com>
2026-01-27 15:33:33 +0100

    refactor: chore member handler uses packet handler

--- server/Networking/Handlers/ChoreMemberHandler.cs
+++ server/Networking/Handlers/ChoreMemberHandler.cs
@@ -9,58 +9,36 @@ using Shared.Networking.Packets;
 
 namespace Networking.Handlers;
 
-public class ChoreMemberHandler(ChoreMemberService service) : IPacketHandler
+public class ChoreMemberHandler(ChoreMemberService service) : PacketHandler
 {
-    public async Task<bool> HandleAsync(ClientContext context, ReadPacket packet, CancellationToken token)
+    protected override async Task<bool> HandleCodesAsync
+        (ClientContext context, ReadPacket packet, CancellationToken token = default)
     {
-        while (!token.IsCancellationRequested)
+        //todo: maybe send a responce
+        if (context.CurrentUser is null)
+            return false;
+        switch (packet.code)
         {
-            if (context.CurrentUser is null)
+            case OpCode.AddMember:
+                return await HandlePacketAsync<AddMembersRequest>(context, packet, req =>
+                        service.AddMembersAsync(context.CurrentUser.Id, req, token), token);
+            case OpCode.DeleteMember:
+                return await HandlePacketAsync<DeleteMemberRequest>(context, packet, req =>
+                        service.DeleteMemberAsync(context.CurrentUser.Id, req, token), token);
+            case OpCode.SetAdminStatus:
+                return await HandlePacketAsync<SetAdminStatusRequest>(context, packet, req =>
+                        service.SetAdminStatusAsync(context.CurrentUser.Id, req, token), token);
+            default:
                 return false;
-            switch (packet.code)
-            {
-                case OpCode.AddMember:
-                    return await Handle<AddMembersRequest>(context, packet, req =>
-                            service.AddMembersAsync(context.CurrentUser.Id, req, token), token);
-                case OpCode.DeleteMember:
-                    return await Handle<DeleteMemberRequest>(context, packet, req =>
-                            service.DeleteMemberAsync(context.CurrentUser.Id, req, token), token);
-                case OpCode.SetAdminStatus:
-                    return await Handle<SetAdminStatusRequest>(context, packet, req =>
-                            service.SetAdminStatusAsync(context.CurrentUser.Id, req, token), token);
-                default:
-                    return false;
-            }
         }
-        return false;
     }
 
-    public OpCode[] GetHandledCodes()
+    public override OpCode[] GetHandledCodes()
     {
         return [
             OpCode.AddMember,
             OpCode.DeleteMember,
             OpCode.SetAdminStatus,
         ];
-    }
-
-    private async Task<bool> Handle<Req, Res>
-    (ClientContext context, ReadPacket packet, Func<Req, Task<Result<Res>>> func, CancellationToken token)
-    where Req : Request
-    {
-        return await Handle(context, packet, func, token);
-    }
-
-    private async Task<bool> Handle<Req>
-        (ClientContext context, ReadPacket packet, Func<Req, Task<Result>> func, CancellationToken token)
-        where Req : Request
-    {
-        var request = JsonSerializer.Deserialize<Req>(packet.jsonData);
-        Debug.Assert(request is not null);
-        SendPacket<Result> sendPacket;
-        var result = await func.Invoke(request);
-        sendPacket = new(packet.code, result);
-        await PacketProtocol.SendPacketAsync(context.Stream, sendPacket);
-        return true;
     }
 }
