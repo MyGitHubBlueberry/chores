0596b56299c134920ff544f9ef51551844d67bc9
MyGitHubBlueberry <namespaceblueberry@gmail.com>
2026-01-27 13:34:52 +0100

    feat: chore member handler setup

--- server/Networking/Handlers/ChoreMemberHandler.cs
+++ server/Networking/Handlers/ChoreMemberHandler.cs
@@ -1,11 +1,9 @@
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Database.Services;
-using Shared.Database.Models;
 using Shared.Networking;
 using Shared.Networking.Packets;
 
@@ -22,12 +20,38 @@ public class ChoreMemberHandler(ChoreMemberService service) : IPacketHandler
             switch (packet.code)
             {
                 case OpCode.AddMember:
+                    return await Handle<AddMembersRequest>(context, packet, req =>
+                            service.AddMembersAsync(context.CurrentUser.Id, req, token), token);
                 case OpCode.DeleteMember:
+                    return await Handle<DeleteMemberRequest>(context, packet, req =>
+                            service.DeleteMemberAsync(context.CurrentUser.Id, req, token), token);
                 case OpCode.SetAdminStatus:
-                default: 
+                    return await Handle<SetAdminStatusRequest>(context, packet, req =>
+                            service.SetAdminStatusAsync(context.CurrentUser.Id, req, token), token);
+                default:
                     return false;
+            }
         }
-    }
         return false;
+    }
+
+    private async Task<bool> Handle<Req, Res>
+    (ClientContext context, ReadPacket packet, Func<Req, Task<Result<Res>>> func, CancellationToken token)
+    where Req : Request
+    {
+        return await Handle(context, packet, func, token);
+    }
+
+    private async Task<bool> Handle<Req>
+        (ClientContext context, ReadPacket packet, Func<Req, Task<Result>> func, CancellationToken token)
+        where Req : Request
+    {
+        var request = JsonSerializer.Deserialize<Req>(packet.jsonData);
+        Debug.Assert(request is not null);
+        SendPacket<Result> sendPacket;
+        var result = await func.Invoke(request);
+        sendPacket = new(packet.code, result);
+        await PacketProtocol.SendPacketAsync(context.Stream, sendPacket);
+        return true;
     }
 }
