651ee8a26f7cf447f0d256af50b0c9a08da815ea
MyGitHubBlueberry <MyGitHubBlueberry@gmail.com>
2026-02-10 21:49:15 +0000

    feat

--- server/Networking/Handlers/AuthHandler.cs
+++ server/Networking/Handlers/AuthHandler.cs
@@ -1,19 +1,22 @@
 using System;
-using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 using Database.Services;
+using Microsoft.Extensions.DependencyInjection;
 using Shared.Database.Models;
 using Shared.Networking;
 using Shared.Networking.Packets;
 
 namespace Networking.Handlers;
 
-public class AuthHandler(UserService service) : PacketHandler
+public class AuthHandler(IServiceScopeFactory scopeFactory) : PacketHandler
 {
     protected override async Task<bool> HandleCodesAsync
         (ClientContext context, ReadPacket packet, CancellationToken token = default)
     {
+        using var scope = scopeFactory.CreateScope();
+        var service = scope.ServiceProvider.GetRequiredService<UserService>();
+        
         switch (packet.code)
         {
             case OpCode.Login:
