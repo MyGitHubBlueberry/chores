8709444229acdcf9c098f942b63400428dc6000d
MyGitHubBlueberry <MyGitHubBlueberry@gmail.com>
2026-02-14 19:49:12 +0000

    feat: ui views :)

---
+++ client/Views/ChoreManagementView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace client.Views;
+
+public partial class ChoreManagementView : UserControl
+{
+    public ChoreManagementView()
+    {
+        InitializeComponent();
+    }
+}
