bc6eaa34ee1d5b9ea08136ea4173a1bd50718aa2
MyGitHubBlueberry <MyGitHubBlueberry@gmail.com>
2026-02-28 15:20:57 +0000

    feat: create chore button creates chore now

---
+++ client/Models/CreateChoreModel.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using client.Views;
+using Networking;
+using Shared.Database.Models;
+using Shared.Networking;
+using Shared.Networking.Packets;
+
+namespace client.ViewModels;
+
+public class CreateChoreModel
+{
+    private readonly Client client;
+    private Action<Result<Chore>>? createChoreCallback;
+
+    public CreateChoreModel(Client client)
+    {
+        this.client = client;
+        client.PacketRecieved += HandlePackets;
+    }
+
+    private void HandlePackets(ReadPacket packet)
+    {
+        Console.WriteLine("create chore received packet. Packet code is: " + packet.code);
+        switch (packet.code)
+        {
+            case OpCode.CreateChore:
+                var result = JsonSerializer.Deserialize<Result<Chore>>(packet.jsonData);
+                Console.WriteLine("about to invoke callback");
+                createChoreCallback?.Invoke(result);
+                Console.WriteLine("callback invoked");
+                break;
+        }
+    }
+
+    public async Task CreateChoreAsync(CreateChoreRequest request, Action<Result<Chore>> callback)
+    {
+        createChoreCallback = callback;
+        await client.SendAsync(new SendPacket<CreateChoreRequest>(OpCode.CreateChore, request));
+    }
+}
--- client/ViewModels/CreateChoreViewModel.cs
+++ client/ViewModels/CreateChoreViewModel.cs
@@ -1,5 +1,10 @@
+using System;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Networking;
+using Shared.Database.Models;
+using Shared.Networking;
+using Shared.Networking.Packets;
 
 namespace client.ViewModels;
 
@@ -7,9 +12,35 @@ public partial class CreateChoreViewModel : ViewModelBase
 {
     [ObservableProperty]
     private ChoreSettingsViewModel settingsViewModel;
+    private CreateChoreModel model;
     
     public CreateChoreViewModel(Client client)
     {
+        model = new(client);
         SettingsViewModel = new ChoreSettingsViewModel(client);
+    }
+
+    [RelayCommand]
+    private void SendCreateChoreRequest()
+    {
+        _ = model.CreateChoreAsync(new CreateChoreRequest(
+            SettingsViewModel.ChoreName,
+            SettingsViewModel.Description, 
+            null,
+                SettingsViewModel.StartDate,
+                SettingsViewModel.EndDate,
+                SettingsViewModel.Duration,
+                SettingsViewModel.Interval
+            ), CreateChoreCallback);
+    }
+
+    public void CreateChoreCallback(Result<Chore> result)
+    {
+        Console.Write("Received callback: ");
+        Console.WriteLine(result.IsSuccess 
+            ? "chore created successfully"
+            : result.ErrorMessage);
+        //close on success and show that chore was created successfully
+        //on fail show message and revalidate all fields
     }
 }
